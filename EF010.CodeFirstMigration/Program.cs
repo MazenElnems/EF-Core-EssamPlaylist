1ac5f9ff209f9381c7890315232c4f64a7207a7a
Mazen Elnems <mazenelnems@gmail.com>
2025-04-05 07:15:35 +0200

    change the target .net framework from .net 7.0 to .net 9.0

--- EF010.CodeFirstMigration/Program.cs
+++ EF010.CodeFirstMigration/Program.cs
@@ -7,15 +7,7 @@ namespace EF010.CodeFirstMigration
     {
         public static void Main(string[] args)
         {
-            using (var context = new AppDbContext())
-            {
-                foreach (var item in context.Sections.Include(x => x.Course))
-                {
-                    Console.WriteLine($"Section: {item.SectionName}, " +
-                        $"Course {item.Course.CourseName}");
-                }
-            }
-
+    
             Console.ReadKey();
         }
     }
